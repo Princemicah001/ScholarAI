e069313bf92b97f266e307b89e15a68e0feed2c9
Prince Micah <micahprincemicah001@gmail.com>
2025-12-04 21:12:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4008234344-8e3c7",
+  "appId": "1:1029030651464:web:2c76c9ea4adb021528da7d",
+  "apiKey": "AIzaSyAN06hS3wNvd7l-LXLbOAgv2523q6g_MeY",
+  "authDomain": "studio-4008234344-8e3c7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1029030651464"
+};
